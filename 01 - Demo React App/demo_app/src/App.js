7d6669b85ebbafa5146b4421a9df9d8f964fdf7d
AlejandroCamara <21253054+AlejandroCamara@users.noreply.github.com>
2019-07-07 08:25:24 -0500

    Demo app with static contents

---
+++ 01 - Demo React App/demo_app/src/App.js
@@ -0,0 +1,22 @@
+// *************** App.js file using hooks *************
+
+// Needs to be imported in order to translate JSX into plain JS
+import React from 'react';
+import './App.css';
+import Person from "./Person/Person";
+
+function App() {
+  // JSX notation
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <Person />
+    </div>
+  );
+
+  // Plain JavaScript translation
+  // return React.createElement('div', {className: 'App'}, 
+  //   React.createElement('h1', null, "Hi, I'm a React App"));
+}
+
+export default App;
