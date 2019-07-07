05a70ed8650b64e523976b9c6db26b38e0ca33a0
AlejandroCamara <21253054+AlejandroCamara@users.noreply.github.com>
2019-07-07 08:31:23 -0500

    Dynamic content using JS statements inside JSX

--- 01 - Demo React App/demo_app/src/Person/Person.js
+++ 01 - Demo React App/demo_app/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const person = () => {
+  // Only single line statements or function calls inside {}
   return (
-    <p>I'm a Person</p>
+    <p>I'm a Person {Math.floor(Math.random() * 30)}</p>
   );
 };
 
