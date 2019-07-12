58448836819651f0586764104f3b896b2ced522a
AlejandroCamara <21253054+AlejandroCamara@users.noreply.github.com>
2019-07-12 15:20:58 -0500

    Using State

--- 01 - Demo React App/demo_app/src/AppClass.js
+++ 01 - Demo React App/demo_app/src/AppClass.js
@@ -1,13 +1,42 @@
 // *************** App.js file using traditional classes *************
 
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: "28" },
+      { name: "Camara", age: "22" },
+      { name: "Hee Hee", age: "35" }
+    ]
+  };
+
+  // Needs to be written as a class property to allow 'this' keyword
+  // refer to the class, instead the function.
+  switchNameHandler = () => {
+    console.log("Was clicked!");
+    // Don't change the state directly!!!
+    // this.state.persons[0].name = "Maximus";
+    this.setState({
+      persons: [
+        { name: "Maximus", age: "28" },
+        { name: "Camara", age: "22" },
+        { name: "Hee Hee", age: "35" }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Person name={this.state.persons[0]} age={this.state.persons[0]} />
+        <Person name={this.state.persons[1]} age={this.state.persons[1]}>
+          My hobbies: Videogames
+        </Person>
+        <Person name={this.state.persons[2]} age={this.state.persons[2]} />
       </div>
     );
   }
